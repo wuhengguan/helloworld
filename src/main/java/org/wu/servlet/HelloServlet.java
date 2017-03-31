318a81fe36824b1ea6191e52096298ed2c9b8533
wuhengguan <wuhg@nssccsz.gov.cn>
2017-03-31 09:23:27 +0800

    add HelloServlet.java

---
+++ src/main/java/org/wu/servlet/HelloServlet.java
@@ -0,0 +1,29 @@
+package org.wu.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String name = req.getParameter("name");
+		if(name == null || "".equals(name)) {
+			name = "jenkins";
+		}
+		resp.getWriter().println("<h1>Hello " + name + "!</h1>");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.doGet(req, resp);
+	}
+	
+	
+}
