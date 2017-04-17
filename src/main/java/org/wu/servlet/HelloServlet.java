cc645d8f74c8c69fbdd782445504a8ca966d34a1
wuhengguan <496044027@qq.com>
2017-04-17 15:15:08 +0800

    Update HelloServlet.java
    test fixed

--- src/main/java/org/wu/servlet/HelloServlet.java
+++ src/main/java/org/wu/servlet/HelloServlet.java
@@ -12,7 +12,7 @@ public class HelloServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		String name = reaq.getParameter("name");
+		String name = req.getParameter("name");
 		if(name == null || "".equals(name)) {
 			name = "jenkins";
 		}
